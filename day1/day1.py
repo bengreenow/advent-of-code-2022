22ba6aa9caca245fb9280bd2d77be3ab44a053d9
Ben Greenow <ben.greenow@existec.com>
2022-12-01 13:16:58 +0000

    day1 part2

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,9 @@
-
-
 acc = 0
-
 elfTotals = []
-
 biggest = 0
 
-
 for x in open('./data.txt'):
-
     if x == '\n':
-
         elfTotals.append(acc)
         if acc > biggest:
             biggest = acc
@@ -18,6 +11,11 @@ for x in open('./data.txt'):
         continue
     acc += int(x)
 
+elfTotals.sort(reverse=True)
+top3 = elfTotals[:3]
 
-print(biggest)
+top3Total = 0
+for x in top3:
+    top3Total += x
+print(top3Total)
 
